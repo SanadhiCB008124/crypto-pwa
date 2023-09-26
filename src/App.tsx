b1154a376c33635ab0147bf837971ee6ea575f00
Sanadhi2002 <sanadhi2002@gmail.com>
2023-09-26 23:49:54 +0530

    camera function

--- src/App.tsx
+++ src/App.tsx
@@ -15,36 +15,56 @@ import Login from "./components/Login.tsx";
 import CreateWallet from "./components/CreateWallet.tsx";
 
 
+const constraints = {
+    video: {
+        width: {
+            min: 1280,
+            ideal: 1920,
+            max: 2560,
+        },
+        height: {
+            min: 720,
+            ideal: 1080,
+            max: 1440,
+        },
+        facingMode: 'user'
+    },
+};
+
 function App() {
+    let isMediaAvailable: string = ":( Media features are not available";
+    if ("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices) {
+        isMediaAvailable = ":) Media Capabilities are available";
+        // Requesting for Permission
+        navigator.mediaDevices.getUserMedia({ video: true });
+
+        const initCamera = async () => {
+            const devices = await navigator.mediaDevices.enumerateDevices();
+            debugger;
+            const videoCam = devices[1];
+            const updatedConstraints = {
+                ...constraints,
+                deviceId: {
+                    exact: videoCam.deviceId,
+                },
+            };
+            //Get the Stream Object
+            const videoStream = await navigator.mediaDevices.getUserMedia(
+                updatedConstraints
+            );
+            //Set the stream to the video element on the page
+            setTimeout(function () {
+                alert("Setting video stream");
+                const videoElement: any = document.querySelector("video");
+                videoElement.srcObject = videoStream;
+            }, 1500);
+        };
+        initCamera();
+    }
     return (
+        <video style={{ width: 1280, height: 720 }} autoPlay></video>
 
-        <BrowserRouter>
 
-            <div className="bg-primary w-full overflow-hidden  ">
-                <div className={`${styles.paddingX} ${styles.flexCenter}  `}>
-
-                    <div className={`${styles.boxWidth} `}>
-                        <Navbar/>
-                    </div>
-
-                </div>
-                <MobileNav/>
-
-                <div className={`bg-primary ${styles.flexStart}`}>
-                    <div className={`${styles.boxWidth}`}>
-                        <Routes>
-                            <Route path="/home" element={<Home />} />
-                            <Route path="/send" element={<Send />} />
-                            <Route path="/swap" element={<Swap />} />
-                            <Route path="/buy" element={<Buy />} />
-                            <Route path="/receive" element={<Recieve />} />
-                            <Route path="/login" element={<Login/>} />
-                            <Route path="/createWallet" element={<CreateWallet/>} />
-                        </Routes>
-                    </div>
-                </div>
-            </div>
-        </BrowserRouter>
     );
 }
 
